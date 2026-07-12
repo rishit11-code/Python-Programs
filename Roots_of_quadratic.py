a9db9fdbdfb599d4274a8f9c5bf6f886a0114d3b
Rishit Kumar Singh <rishit.kumarsingh11@gmail.com>
2026-07-13 00:26:36 +0530

    Roots

---
+++ Roots_of_quadratic.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the coefficient of x^2: "))
+b=int(input("Enter the coefficient of x: "))
+c=int(input("Enter the constant term: "))
+r1=(-b+(b**2-4*a*c)**0.5)/(2*a)
+r2=(-b-(b**2-4*a*c)**0.5)/(2*a)
+print("The roots of the quadratic equation are: ",r1," and ",r2)
